8a2b3e300a655e53a9a6ece2368a607ac1afc358
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-16 10:49:56 -0600

    Add DB Schema

---
+++ db/schema.rb
@@ -0,0 +1,92 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_07_16_030812) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "champion_origin_class_types", force: :cascade do |t|
+    t.bigint "champion_id"
+    t.bigint "origin_id"
+    t.bigint "class_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["champion_id"], name: "index_champion_origin_class_types_on_champion_id"
+    t.index ["class_type_id"], name: "index_champion_origin_class_types_on_class_type_id"
+    t.index ["origin_id"], name: "index_champion_origin_class_types_on_origin_id"
+  end
+
+  create_table "champions", force: :cascade do |t|
+    t.string "name"
+    t.integer "rarity_cost"
+    t.boolean "has_passive"
+    t.boolean "has_spell"
+    t.string "ability_description"
+    t.string "model_img"
+    t.string "icon_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "class_types", force: :cascade do |t|
+    t.string "name"
+    t.string "tier1_reqs"
+    t.string "tier2_reqs"
+    t.string "tier3_reqs"
+    t.string "icon_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "trait"
+    t.integer "trait_stat"
+    t.string "icon_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "origins", force: :cascade do |t|
+    t.string "name"
+    t.string "tier1_reqs"
+    t.string "tier2_reqs"
+    t.string "tier3_reqs"
+    t.string "icon_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_items", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_recipe_items_on_item_id"
+    t.index ["recipe_id"], name: "index_recipe_items_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "requirements"
+    t.string "icon_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "champion_origin_class_types", "champions"
+  add_foreign_key "champion_origin_class_types", "class_types"
+  add_foreign_key "champion_origin_class_types", "origins"
+  add_foreign_key "recipe_items", "items"
+  add_foreign_key "recipe_items", "recipes"
+end
