b7e6771ab51f634f4541fdf1f5a5d5e8783afedc
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-16 11:33:13 -0600

    Add realtionships describe block to test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  it {should have_many :recipe_items}
-  it {should have_many(:items).through :recipe_items}
+  describe "Relationships" do
+    it {should have_many :recipe_items}
+    it {should have_many(:items).through :recipe_items}
+  end
 end
