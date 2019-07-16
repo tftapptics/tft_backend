70baa7509df2ec82a0881bcdb5cbd1d4d3ea565e
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-16 11:33:13 -0600

    Add realtionships describe block to test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  it {should have_many :recipe_items}
-  it {should have_many(:recipes).through :recipe_items}
+  describe "Relationships" do
+    it {should have_many :recipe_items}
+    it {should have_many(:recipes).through :recipe_items}
+  end
 end
--- spec/models/origin_spec.rb
+++ spec/models/origin_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Origin, type: :model do
-  it {should have_many :champion_origin_class_types}
-  it {should have_many(:champions).through :champion_origin_class_types}
+  describe "Relationships" do
+    it {should have_many :champion_origin_class_types}
+    it {should have_many(:champions).through :champion_origin_class_types}
+  end
 end
