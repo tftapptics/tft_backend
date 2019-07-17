8bdc7e803ac159446b43e7f18cd5fed88f0f419a
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-17 13:25:24 -0600

    Add Migrations

--- db/migrate/20190716023002_create_class_types.rb
+++ db/migrate/20190716023002_create_class_types.rb
@@ -2,10 +2,10 @@ class CreateClassTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :class_types do |t|
       t.string :name
-      t.string :tier1_reqs
-      t.string :tier2_reqs
-      t.string :tier3_reqs
-      t.string :icon_img
+      t.string :thumbnail
+      t.string :summary
+      t.string :tier_info, array: true, default: []
+      t.integer :tiers, array: true, default: []
 
       t.timestamps
     end
