d3ff03f537d2a72b25edfc03bbd7bac83483fbb9
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-17 13:52:45 -0600

    Recipe migration

--- db/migrate/20190716024040_create_recipes.rb
+++ db/migrate/20190716024040_create_recipes.rb
@@ -2,8 +2,10 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.text :requirements     # possible way to store both items (array: true, default: [])
-      t.string :icon_img
+      t.string :item_1
+      t.string :item_2
+      t.string :description
+      t.string :thumbnail
 
       t.timestamps
     end
