3087c9131a48bae210755ad61370383908117324
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-17 13:26:25 -0600

    Add spec changes

--- spec/models/raw_item_spec.rb
+++ spec/models/raw_item_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
+RSpec.describe RawItem, type: :model do
   describe "Relationships" do
     it {should have_many :recipe_items}
     it {should have_many(:recipes).through :recipe_items}
--- spec/models/recipe_item_spec.rb
+++ spec/models/recipe_item_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe RecipeItem, type: :model do
   describe "Relationships" do
     it {should belong_to(:recipe)}
-    it {should belong_to(:item)}
+    it {should belong_to(:raw_item)}
   end
 end
