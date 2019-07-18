085ced5b657b6a467541eefff3db92760522b278
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-18 15:01:42 -0600

    Edit Migrations
    Co-authored-by: Ethan Grab <40487417+Stoovles@users.noreply.github.com>

--- db/migrate/20190716030023_create_champion_origin_class_types.rb
+++ db/migrate/20190716030023_create_champion_origin_class_types.rb
@@ -2,8 +2,7 @@ class CreateChampionOriginClassTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :champion_origin_class_types do |t|
       t.references :champion, foreign_key: true
-      t.references :origin, foreign_key: true
-      t.references :class_type, foreign_key: true
+      t.references :origin_class_type, foreign_key: true
 
       t.timestamps
     end
