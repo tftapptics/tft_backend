47846894b338b1c4dddc213bb1010908fdf9089f
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-21 14:55:20 -0600

    Add RawItem Index and Show routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :champions, only: [:show,:index]
+      resources :champions, only: [:show, :index]
       resources :origin_class_type, only: [:show, :index]
+      resources :raw_items, only: [:show, :index]
     end
   end
 
